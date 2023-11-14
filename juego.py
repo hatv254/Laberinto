4c4a1bce9281ac2d81c1d5551c07aa3b2199e738
Hector Turcios <hectorturcios254@gmail.com>
2023-11-14 17:24:46 -0600

    Agregando cambios finales

--- juego.py
+++ juego.py
@@ -1,39 +1,29 @@
-"""
-posiciones inicial y final son atributos de esta clase, reescribe todas tus funciones 
-anteriores de forma que sean métodos de la clase y todo esté encapsulado.
-
-"""
 from pynput import keyboard as bk
-import os
+import os, random, re
 
 class Juego:
-    global inicial 
-    global final
-
-    def __init__(self) -> None:
+    def __init__(self, posicion_inicial:[], posicion_final:[], laberinto:list) -> None:
+        self.posicion_final = posicion_final
+        self.laberinto = laberinto
+        self.posicion_personaje = posicion_inicial  # Iniciar en la fila 1
         listener = bk.Listener(on_release=self.on_key_release)
         listener.start()
         self.mostrarLaberinto()
         listener.join()
 
-    posicion_personaje = [0, 0]
-
-    def mostrarLaberinto():
-        global laberinto
+    def mostrarLaberinto(self):
         # Limpia la pantalla
         os.system('cls' if os.name == 'nt' else 'clear')
-        for i in range(fila):
-            for x in range(columna):
-                if posicion_personaje == [i, x]:
+        for i in range(len(self.laberinto)):
+            for x in range(len(self.laberinto[0])):
+                if self.posicion_personaje == [i, x]:
                     print("P", end="")
                 else:
-                    print(laberinto[i][x], end="")
+                    print(self.laberinto[i][x], end="")
             print("")
 
-
     def moverPersonaje(self, key):
-        global posicion_personaje
-        nueva_posicion = list(posicion_personaje)
+        nueva_posicion = self.posicion_personaje
         if key == bk.Key.up:
             nueva_posicion[0] -= 1
         elif key == bk.Key.down:
@@ -43,18 +33,52 @@ class Juego:
         elif key == bk.Key.right:
             nueva_posicion[1] += 1
         # Verificar si la nueva posición es válida
-        if (0 <= nueva_posicion[0] < fila
-            and 0 <= nueva_posicion[1] < columna
-            and laberinto[nueva_posicion[0]][nueva_posicion[1]] == "."):
-            posicion_personaje = nueva_posicion
+        if (
+            0 <= nueva_posicion[0] < len(self.laberinto)
+            and 0 <= nueva_posicion[1] < len(self.laberinto[0])
+            and self.laberinto[nueva_posicion[0]][nueva_posicion[1]] == "."
+        ):
+            self.posicion_personaje = nueva_posicion
             self.mostrarLaberinto()
             self.verificar_final()
 
-    def verificar_final():
-        global fila, columna, posicion_personaje
-        if posicion_personaje == [fila - 1, columna - 1]:
+    def verificar_final(self):
+        if self.posicion_personaje == self.posicion_final:
             exit()  # Finaliza el programa
 
     def on_key_release(self, key):
         if key in [bk.Key.up, bk.Key.down, bk.Key.left, bk.Key.right]:
             self.moverPersonaje(key)
+
+
+class JuegoArchivo(Juego):
+    def __init__(self) -> None:
+        laberinto = self.randomLaberinto()
+        posicion_inicial = self.coordeneadasIniciales(laberinto)
+        posicion_final = self.final(laberinto)
+        laberinto = self.quitarPrimeraFila(laberinto)
+        super().__init__(posicion_inicial=posicion_inicial, posicion_final=posicion_final, laberinto=laberinto)
+
+    def quitarPrimeraFila(self, laberinto):
+        return laberinto[1:]
+
+
+    def coordeneadasIniciales(self, laberinto):
+        coordenadasInicio = [int(laberinto[0][0]), int(laberinto[0][2])]  # Convertir a int
+        return coordenadasInicio
+
+    def final(self, laberinto):
+        coordenadasFinal = [int(laberinto[0][4:6]), int(laberinto[0][7:9])]  # Convertir a int
+        return coordenadasFinal
+
+    def randomLaberinto(self):
+        lista_Laberinto = os.listdir("Laberintos")
+        laberinto = random.sample(lista_Laberinto, 1)
+        # Leemos el archivo
+        with open(f"Laberintos/{laberinto[0]}", 'r') as laberirnto:
+            laberinto = list(laberirnto)
+        # Limpiamos el mapa
+        laberinto_limpio = [re.sub(r'[\n\[\],]', '', elemento) for elemento in laberinto]
+        return laberinto_limpio
+
+juego = JuegoArchivo()
